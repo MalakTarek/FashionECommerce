d34db59dbd630289472f4c3543f07d47753a99ba
mariamelokaby <mariamokby@gmail.com>
2024-05-18 17:41:52 +0300

    Update products_page.dart

--- lib/products_page.dart
+++ lib/products_page.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart'; // Add this import
+import 'package:firebase_storage/firebase_storage.dart';
 import 'product.dart';
 
 class ProductsPage extends StatefulWidget {
@@ -33,6 +35,70 @@ class _ProductsPageState extends State<ProductsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Products'),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Navigator.push(
+              // context,
+              //MaterialPageRoute(builder: (context) => CartPage()), // replace with your CartPage
+              //);
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                /* Navigator.push(
+                  context,
+                  //MaterialPageRoute(builder: (context) => ProfilePage()), // replace with your ProfilePage
+                );*/
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Wishlist'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                /*Navigator.push(
+                  context,
+                  //MaterialPageRoute(builder: (context) => WishlistPage()), // replace with your WishlistPage
+                );*/
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
       ),
       body: FutureBuilder<List<Product>>(
         future: _allProductsFuture,
@@ -62,24 +128,41 @@ class _ProductsPageState extends State<ProductsPage> {
                     onTap: () {
                       //Navigator.push(context, MaterialPageRoute(builder: (context) => AnotherPage()));
                     },
-                    child: GridTile(
-                      child: Image.network(products[index].imageUrl),
-                      footer: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 60.0, vertical: 0.1), // Adjust the vertical padding as needed
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(products[index].name),
-                            Text('Price: ${products[index].price.toString()}'),
-                          ],
-                        ),
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded( // Wrap with Expanded
+                            child: Center(
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 150,
+                                child: Image.network(
+                                  products[index].imageUrl,
+                                  fit: BoxFit.contain, // Change the fit property
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(products[index].name),
+                                Text('Price: ${products[index].price.toString()}'),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
+
+
+
                   );
                 },
               );
-
-
             }
           }
         },
@@ -94,97 +177,111 @@ class _ProductsPageState extends State<ProductsPage> {
   }
 
   void _showCreateProductBottomSheet(BuildContext context) async {
-    final TextEditingController _productName = TextEditingController();
-    final TextEditingController _vendorName = TextEditingController();
-    final TextEditingController _price = TextEditingController();
-    final TextEditingController _category = TextEditingController();
-    final ImagePicker _picker = ImagePicker();
-    String? _imagePath;
 
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          padding: EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Create Product',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot doc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      bool isVendor = doc['role'] == 'vendor'; // Assuming 'role' field in user document
+      if (isVendor) {
+        final TextEditingController _productName = TextEditingController();
+        final TextEditingController _vendorName = TextEditingController();
+        final TextEditingController _price = TextEditingController();
+        final TextEditingController _category = TextEditingController();
+        final ImagePicker _picker = ImagePicker();
+        String? _imagePath;
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Create Product',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _productName,
+                      decoration: InputDecoration(labelText: 'Product Name'),
+                    ),
+                    TextField(
+                      controller: _vendorName,
+                      decoration: InputDecoration(labelText: 'Vendor Name'),
+                    ),
+                    TextField(
+                      controller: _price,
+                      decoration: InputDecoration(labelText: 'Price'),
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextField(
+                      controller: _category,
+                      decoration: InputDecoration(labelText: 'Category'),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final XFile? pickedImage = await _picker.pickImage(
+                            source: ImageSource.gallery);
+                        if (pickedImage != null) {
+                          _imagePath = pickedImage.path;
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('Image selected: $_imagePath'),
+                            duration: Duration(seconds: 2),
+                          ));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text('No image selected.'),
+                            duration: Duration(seconds: 2),
+                          ));
+                        }
+                      },
+                      child: Text('Select Image'),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_imagePath != null && _productName.text.isNotEmpty &&
+                            _vendorName.text.isNotEmpty && _price.text.isNotEmpty &&
+                            _category.text.isNotEmpty) {
+                          // Upload image to Firebase Storage
+                          final imageUrl = await _uploadImageToStorage(_imagePath!);
+                          await _createProduct(
+                            context,
+                            _productName.text.trim(),
+                            _vendorName.text.trim(),
+                            double.parse(_price.text.trim()),
+                            _category.text.trim(),
+                            imageUrl,
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                                'Please fill all fields and select an image.'),
+                            duration: Duration(seconds: 2),
+                          ));
+                        }
+                      },
+                      child: Text('Create'),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: _productName,
-                  decoration: InputDecoration(labelText: 'Product Name'),
-                ),
-                TextField(
-                  controller: _vendorName,
-                  decoration: InputDecoration(labelText: 'Vendor Name'),
-                ),
-                TextField(
-                  controller: _price,
-                  decoration: InputDecoration(labelText: 'Price'),
-                  keyboardType: TextInputType.number,
-                ),
-                TextField(
-                  controller: _category,
-                  decoration: InputDecoration(labelText: 'Category'),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    final XFile? pickedImage = await _picker.pickImage(
-                        source: ImageSource.gallery);
-                    if (pickedImage != null) {
-                      _imagePath = pickedImage.path;
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('Image selected: $_imagePath'),
-                        duration: Duration(seconds: 2),
-                      ));
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('No image selected.'),
-                        duration: Duration(seconds: 2),
-                      ));
-                    }
-                  },
-                  child: Text('Select Image'),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_imagePath != null && _productName.text.isNotEmpty &&
-                        _vendorName.text.isNotEmpty && _price.text.isNotEmpty &&
-                        _category.text.isNotEmpty) {
-                      // Upload image to Firebase Storage
-                      final imageUrl = await _uploadImageToStorage(_imagePath!);
-                      await _createProduct(
-                        context,
-                        _productName.text.trim(),
-                        _vendorName.text.trim(),
-                        double.parse(_price.text.trim()),
-                        _category.text.trim(),
-                        imageUrl,
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(
-                            'Please fill all fields and select an image.'),
-                        duration: Duration(seconds: 2),
-                      ));
-                    }
-                  },
-                  child: Text('Create'),
-                ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         );
-      },
-    );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+
+
+          content: Text('Only vendors can create products.'),
+          duration: Duration(seconds: 2),
+        ));
+      }
+    }
   }
 
   Future<String> _uploadImageToStorage(String imagePath) async {
@@ -217,13 +314,12 @@ class _ProductsPageState extends State<ProductsPage> {
       price: price,
       comments: [],
       overallRating: 0.0,
-<<<<<<< Updated upstream
-      category: category, ratings: [],
-=======
       category: category,
       ratings: [],
       description: '',
->>>>>>> Stashed changes
+      sizes: [],
+      unitsByColorAndSize: {},
+
     );
 
     try {
