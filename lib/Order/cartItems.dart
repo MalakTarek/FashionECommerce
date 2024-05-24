a5e75dad652ac34f678541b4526bf9da7d559d24
mariamelokaby <mariamokby@gmail.com>
2024-05-25 00:23:33 +0300

    Create cartItems.dart

---
+++ lib/Order/cartItems.dart
@@ -0,0 +1,16 @@
+
+class CartItem {
+  final String imageUrl;
+  final double price;
+  final String name;
+
+  CartItem({
+    required this.imageUrl,
+    required this.price,
+    required this.name});
+}
+
+//This page is only to define the attributes I need. thank u
+
+
+
